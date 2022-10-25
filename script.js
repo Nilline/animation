e53d69644f7a78d5b2ea14c55136bd9b90d81d49
Nilline <74991442+Nilline@users.noreply.github.com>
2022-10-26 00:37:24 +0300

    Add interactive mouse

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ window.addEventListener("load", () => {
 	data-decor-item - Пишем для елементов которые нужно анимировать внутри родителя с атрибутом data-decor-body.
 	data-decor-min-speed="" - Устанавливает минимальное значение анимации в пикселях (можно дробные значения). Добавлять для блока с аттрибутом data-decor-body (Не обязательный атрибут, по умолчанию будет значение 0.6).
 	data-decor-max-speed="" - Устанавливает максимальное значение анимации в пикселях (можно дробные значения). Добавлять для блока с аттрибутом data-decor-body (Не обязательный атрибут, по умолчанию будет значение 1).
+	data-decor-truck-delay="" - Устанавливает значение задержки исчезновения класса после касания елемента к краю экрана (Указывать в милисекундах). Добавлять для блока с аттрибутом data-decor-body (Не обязательный атрибут, по умолчанию будет значение 200).
 	*/
 	class SetAnimateDecorItems {
 		constructor(...props) {
-			// Основное хранилище информации, не трогать!!! заполняется автоматически и изменяется в процессе работы!
 			this.config = {
+				// Основные настройки
+				settings: {
+					// Нужно ли инициировать анимацию
+					init: true,
+
+					// Включение события взаимодействия с мышкой
+					mouseInit: true,
+					// Скорость замедления
+					speedReduction: 0.5,
+					// Задержка замедления
+					delayReduction: 300,
+					// Минимальная скорость ускорения
+					minValueAcceleration: 3,
+					// Максимальная скорость ускорения
+					maxValueAcceleration: 10,
+
+					/*
+					Нужно ли добавлять класс при касании к стенкам
+					Классы которые добавляются при касании к разным краям блока:
+					_truck-bottom, _truck-top, _truck-left, _truck-right
+					*/
+					onAddClassTruckItem: true,
+				},
+
+				// Основное хранилище информации, не трогать!!! заполняется автоматически и изменяется в процессе работы!
 				decorBody: null,
 				items: [],
 				posItemsX: [],
@@ -18,18 +43,16 @@ window.addEventListener("load", () => {
 				speedDecorY: [],
 				transformPosX: [],
 				transformPosY: [],
+				speedModifierX: [],
+				speedModifierY: [],
+				intervalActive: {
+					x: false,
+					y: false
+				},
 				minSpeed: props[0] && props[0] <= props[1] ? props[0] : 0.6,
 				maxSpeed: props[1] && props[1] >= props[0] ? props[1] : 1,
 				delayRemoveClassTruck: props[2] ? props[2] : 200,
 
-				// Нужно ли инициировать анимацию
-				init: true,
-				// Нужно ли добавлять класс при касании к стенкам
-				/*
-				Классы которые добавляются при касании к разным краям блока:
-				_truck-y-bottom, _truck-y-top, _truck-x-left, _truck-x-right
-				*/
-				onAddClassTruckItem: true,
 
 				classess: {
 					body: '[data-decor-body]',
@@ -38,13 +61,14 @@ window.addEventListener("load", () => {
 				}
 			};
 			this.setAnimate = this.setAnimate.bind(this);
-			this.config.init ? this.initAnim() : null;
+			this.mouseEvent = this.mouseEvent.bind(this);
+			this.config.settings.init ? this.initAnim() : null;
 		}
 		// Получение рандомного числа для скорости анимации элемента
 		getRandomNum = negative => {
 			const min = this.config.minSpeed,
 				max = this.config.maxSpeed;
-			return negative ? +(Math.random() * (-max - -min) + -min).toFixed(2) : +(Math.random() * (max - min) + min).toFixed(2);
+			return negative ? (Math.round((Math.random() * (-max - -min) + -min) * 100) / 100) : Math.round((Math.random() * (max - min) + min) * 100) / 100;
 		}
 
 		// Функция инициализации
@@ -63,7 +87,15 @@ window.addEventListener("load", () => {
 						const decorItems = this.config.decorBody.querySelectorAll(this.config.classess.items);
 
 						if (decorItems.length) {
-							decorItems.forEach(item => this.config.items.push(item));
+							decorItems.forEach((item, i) => {
+								this.config.items.push(item);
+								item.dataset.decorIndex = i;
+								if (this.config.settings.mouseInit) {
+									this.config.speedModifierX.push(1);
+									this.config.speedModifierY.push(1);
+								}
+							});
+							this.config.settings.mouseInit ? decorBodyArr[i].addEventListener("mousemove", this.mouseEvent) : '';
 						}
 						break;
 					}
@@ -96,17 +128,93 @@ window.addEventListener("load", () => {
 			this.config.items.length ? this.setAnimate() : null;
 		}
 
+		reductionNumber() {
+			if (!this.config.intervalActive.x) {
+				this.config.intervalActive.x = true;
+				const arrX = this.config.speedModifierX;
+				let timer = setInterval(() => {
+					arrX.forEach((num, i) => {
+						if (num > 1) {
+							arrX.splice(i, 1, num - this.config.settings.speedReduction);
+						}
+					})
+					if (!this.config.speedModifierX.filter(item => item > 1).length) {
+						clearInterval(timer);
+						this.config.intervalActive.x = false;
+					}
+				}, this.config.settings.delayReduction);
+			}
+			if (!this.config.intervalActive.y) {
+				this.config.intervalActive.y = true;
+				const arrY = this.config.speedModifierY;
+				let timer = setInterval(() => {
+					arrY.forEach((num, i) => {
+						if (num > 1) {
+							arrY.splice(i, 1, num - this.config.settings.speedReduction);
+						}
+					})
+					if (!this.config.speedModifierY.filter(item => item > 1).length) {
+						clearInterval(timer);
+						this.config.intervalActive.y = false;
+					}
+				}, this.config.settings.delayReduction);
+			}
+		}
+
+		mouseEvent(e) {
+			if (this.config.items.length) {
+				if (e.target.closest(this.config.classess.items)) {
+					const target = e.target.closest(this.config.classess.items),
+						left = +(target.getBoundingClientRect().left).toFixed(0),
+						right = +(target.getBoundingClientRect().left + target.clientWidth).toFixed(0),
+						top = +(target.getBoundingClientRect().top).toFixed(0),
+						bottom = +(target.getBoundingClientRect().top + target.offsetHeight).toFixed(0),
+						width = right - left,
+						height = bottom - top,
+						i = target.dataset.decorIndex,
+						minValueAcceleration = this.config.settings.minValueAcceleration,
+						maxValueAcceleration = this.config.settings.maxValueAcceleration;
+
+					if ((left + width / 2) > e.x) {
+						this.config.posItemsX.splice(i, 1, false);
+						this.config.speedDecorX.splice(i, 1, this.getRandomNum());
+						this.config.speedModifierX.splice(i, 1, +(Math.random() * (maxValueAcceleration - minValueAcceleration) + minValueAcceleration).toFixed(0));
+						this.reductionNumber();
+					}
+					if ((right - width / 2) < e.x) {
+						this.config.posItemsX.splice(i, 1, true);
+						this.config.speedDecorX.splice(i, 1, this.getRandomNum('negative'));
+						this.config.speedModifierX.splice(i, 1, +(Math.random() * (maxValueAcceleration - minValueAcceleration) + minValueAcceleration).toFixed(0));
+						this.reductionNumber();
+					}
+					if ((top + height / 2) > e.y) {
+						this.config.posItemsY.splice(i, 1, false);
+						this.config.speedDecorY.splice(i, 1, this.getRandomNum());
+						this.config.speedModifierY.splice(i, 1, +(Math.random() * (maxValueAcceleration - minValueAcceleration) + minValueAcceleration).toFixed(0));
+						this.reductionNumber();
+					}
+					if ((bottom - height / 2) < e.y) {
+						this.config.posItemsY.splice(i, 1, true);
+						this.config.speedDecorY.splice(i, 1, this.getRandomNum('negative'));
+						this.config.speedModifierY.splice(i, 1, +(Math.random() * (10 - 3) + 3).toFixed(0));
+						this.reductionNumber();
+					}
+				}
+			}
+
+		}
+
 		// Функция добавления класса при касании (можно отключать в настройках скрипта в конструкторе)
 		itemTruck(item, side) {
 			if (side === 'x-left' || side === 'x-right') {
-				item.classList.add(side === 'x-left' ? '_truck-x-left' : '_truck-x-right');
+				item.classList.add(side === 'x-left' ? '_truck-left' : '_truck-right');
 				setTimeout(() => {
-					item.classList.remove('_truck-x-left', '_truck-x-right');
+					item.classList.remove('_truck-left', '_truck-right');
 				}, this.config.delayRemoveClassTruck);
 			} else if (side === 'y-bottom' || side === 'y-top') {
-				item.classList.add(side === 'y-bottom' ? '_truck-y-bottom' : '_truck-y-top');
+				item.classList.add(side === 'y-bottom' ? '_truck-bottom' : '_truck-top');
 				setTimeout(() => {
-					item.classList.remove('_truck-y-bottom', '_truck-y-top');
+					item.classList.remove('_truck-bottom', '_truck-top');
 				}, this.config.delayRemoveClassTruck);
 			}
 		}
@@ -120,33 +228,34 @@ window.addEventListener("load", () => {
 				if (!this.config.posItemsX[i]) {
 					if (valueX >= (this.config.decorBody.clientWidth - item.getBoundingClientRect().width)) {
 						this.config.posItemsX.splice(i, 1, true);
-						this.config.speedDecorX.splice(i, 1, this.getRandomNum('negative'))
-						this.config.onAddClassTruckItem ? this.itemTruck(item, 'x-right') : '';
+						this.config.speedDecorX.splice(i, 1, this.getRandomNum('negative'));
+						this.config.settings.onAddClassTruckItem ? this.itemTruck(item, 'x-right') : '';
 					}
-					this.config.transformPosX.splice(i, 1, this.config.transformPosX[i] + this.config.speedDecorX[i]);
+					this.config.transformPosX.splice(i, 1, +((Math.round(this.config.transformPosX[i] * 100) / 100) + (this.config.speedDecorX[i] * this.config.speedModifierX[i])).toFixed(2));
 				} else if (this.config.posItemsX[i]) {
 					if (valueX <= 0) {
 						this.config.posItemsX.splice(i, 1, false);
-						this.config.speedDecorX.splice(i, 1, this.getRandomNum())
-						this.config.onAddClassTruckItem ? this.itemTruck(item, 'x-left') : '';
+						this.config.speedDecorX.splice(i, 1, this.getRandomNum());
+						this.config.settings.onAddClassTruckItem ? this.itemTruck(item, 'x-left') : '';
 					}
-					this.config.transformPosX.splice(i, 1, this.config.transformPosX[i] + this.config.speedDecorX[i]);
+					this.config.transformPosX.splice(i, 1, +((Math.round(this.config.transformPosX[i] * 100) / 100) + this.config.speedDecorX[i] * this.config.speedModifierX[i]).toFixed(2));
 				}
 				if (!this.config.posItemsY[i]) {
 					if (valueY >= (this.config.decorBody.clientHeight - item.offsetHeight)) {
 						this.config.posItemsY.splice(i, 1, true);
-						this.config.speedDecorY.splice(i, 1, this.getRandomNum('negative'))
-						this.config.onAddClassTruckItem ? this.itemTruck(item, 'y-bottom') : '';
+						this.config.speedDecorY.splice(i, 1, this.getRandomNum('negative'));
+						this.config.settings.onAddClassTruckItem ? this.itemTruck(item, 'y-bottom') : '';
 					}
-					this.config.transformPosY.splice(i, 1, this.config.transformPosY[i] + this.config.speedDecorY[i]);
+					this.config.transformPosY.splice(i, 1, +((Math.round(this.config.transformPosY[i] * 100) / 100) + (this.config.speedDecorY[i] * this.config.speedModifierY[i])).toFixed(2));
 				} else if (this.config.posItemsY[i]) {
 					if (valueY <= 0) {
 						this.config.posItemsY.splice(i, 1, false);
-						this.config.speedDecorY.splice(i, 1, this.getRandomNum())
-						this.config.onAddClassTruckItem ? this.itemTruck(item, 'y-top') : '';
+						this.config.speedDecorY.splice(i, 1, this.getRandomNum());
+						this.config.settings.onAddClassTruckItem ? this.itemTruck(item, 'y-top') : '';
 					}
-					this.config.transformPosY.splice(i, 1, this.config.transformPosY[i] + this.config.speedDecorY[i]);
+					this.config.transformPosY.splice(i, 1, +((Math.round(this.config.transformPosY[i] * 100) / 100) + (this.config.speedDecorY[i] * this.config.speedModifierY[i])).toFixed(2));
 				}
+
 				item.style.cssText += `transform: translate3d(${this.config.transformPosX[i]}px, ${this.config.transformPosY[i]}px, 0);`;
 			})
 			requestAnimationFrame(this.setAnimate);
